9f51eb753edcf4b72ed572511b0c0b1249479975
CapitanFindusFI <am95,dev@gmail.com>
2020-04-05 13:21:18 +0200

    updated tests

--- tests/generators/query-generator.test.ts
+++ tests/generators/query-generator.test.ts
@@ -4,12 +4,24 @@ import GraphqlQueryGenerator from "../../src/generator/graphql-query.generator";
 const generator = new GraphqlQueryGenerator();
 
 describe('query generator class test suite', () => {
-    it('should correctly create a graph query with a simple request', () => {
+    it('should correctly create a graph query with a simple request without params', () => {
         const requests: IGraphQLRequest[] = [{
             fragmentName: 'users',
             fragmentFields: ['id', 'name', 'surname']
         }];
         const query: string = generator.generateRequestString(requests);
         expect(query).toBe('query{\nusers{id name surname}}');
+    });
+
+    it('should correctly generate a graph query with multiple fragments',() => {
+       const requests: IGraphQLRequest[] = [{
+           fragmentName:'users',
+           fragmentFields: ['id','name','surname']
+       },{
+           fragmentName:'accounts',
+           fragmentFields: ['id','createdAt']
+       }];
+       const query: string = generator.generateRequestString(requests);
+       expect(query).toBe('query{\nusers{id name surname}\naccounts{id createdAt}}');
     });
 });
