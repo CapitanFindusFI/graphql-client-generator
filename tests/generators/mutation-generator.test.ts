7ef22880fae899e1aa805652cbe8cd558517f5a7
CapitanFindusFI <am95,dev@gmail.com>
2020-04-05 20:22:49 +0200

    created test suite for mutation generator

---
+++ tests/generators/mutation-generator.test.ts
@@ -0,0 +1,60 @@
+import {IGraphQLRequest} from "../../src/interfaces/graphql-request.interface";
+import {ValidationError} from "../../src/exceptions/validation.error";
+import GraphqlMutationGenerator from "../../src/generator/graphql-mutation.generator";
+
+const generator = new GraphqlMutationGenerator();
+
+describe('query generator class test suite', () => {
+    it('should correctly create a graph query with a simple request without params', () => {
+        const requests: IGraphQLRequest[] = [{
+            fragmentName: 'users',
+            fragmentFields: ['id', 'name', 'surname']
+        }];
+        const query: string = generator.generateRequestString(requests);
+        expect(query).toBe('mutation{\nusers{id name surname}}');
+    });
+
+    it('should correctly generate a graph query with multiple fragments', () => {
+        const requests: IGraphQLRequest[] = [{
+            fragmentName: 'users',
+            fragmentFields: ['id', 'name', 'surname']
+        }, {
+            fragmentName: 'accounts',
+            fragmentFields: ['id', 'createdAt']
+        }];
+        const query: string = generator.generateRequestString(requests);
+        expect(query).toBe('mutation{\nusers{id name surname}\naccounts{id createdAt}}');
+    });
+
+    it('should throw an error for incorrect values', () => {
+        const requests: IGraphQLRequest[] = [{
+            fragmentName: 'users',
+            fragmentParams: [{
+                alias: '$id',
+                type: 'String',
+                name: 'id'
+            }],
+            fragmentFields: ['name', 'surname'],
+            fragmentValues: {}
+        }];
+
+        expect(() => generator.generateRequestString(requests)).toThrow(ValidationError);
+    });
+
+    it('should throw an error for missing values', () => {
+        const requests: IGraphQLRequest[] = [{
+            fragmentName: 'users',
+            fragmentParams: [{
+                alias: '$id',
+                type: 'String',
+                name: 'id'
+            }],
+            fragmentFields: ['name', 'surname'],
+            fragmentValues: {
+                id: '123'
+            }
+        }];
+
+        expect(() => generator.generateRequestString(requests)).toThrow(Error);
+    });
+});
